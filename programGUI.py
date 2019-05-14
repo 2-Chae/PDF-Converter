5e32ed10256ea9c942030281523c800910b389f5
Chaehyeon Lee <123456ccdd@naver.com>
2019-05-14 16:59:12 +0900

    Update programGUI.py
    큰 틀 생성.

--- programGUI.py
+++ programGUI.py
@@ -1,10 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QDesktopWidget
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QWidget, 
+QPushButton, QDesktopWidget, QHBoxLayout, QGridLayout, QLabel, QLineEdit, QTextEdit)
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import QCoreApplication, Qt
 
 
-class MyApp(QMainWindow, QWidget):
+class MyApp(QWidget):
 
     def __init__(self):
         super().__init__()
@@ -12,12 +13,36 @@ class MyApp(QMainWindow, QWidget):
         self.initUI()
 
     def initUI(self):
-        btn = QPushButton('Quit', self)
-        btn.move(230, 148)
-        btn.resize(btn.sizeHint())
-        btn.clicked.connect(QCoreApplication.instance().quit)
+        # set grid layout
+        grid = QGridLayout()
+        self.setLayout(grid)
 
-        self.statusBar().showMessage('ready')
+
+        # fromBox = QHBoxLayout()
+        # fromBox.addStretch()
+        
+        from_label = QLabel('From')
+        from_label.setAlignment(Qt.AlignCenter)
+        to_label = QLabel('to')
+        to_label.setAlignment(Qt.AlignCenter)
+
+        grid.addWidget(from_label, 0, 0)
+        grid.addWidget(to_label, 1, 0)
+        
+
+        quit_btn = QPushButton('quit')
+        quit_btn.clicked.connect(QCoreApplication.instance().quit)
+        #btn.resize(30, 10)
+
+        grid.addWidget(quit_btn, 2, 2)
+
+        grid.addWidget(QLineEdit(), 0, 1);
+        grid.addWidget(QLineEdit(), 1, 1);
+
+        open_btn = QPushButton('open')
+        grid.addWidget(open_btn, 0, 2)
+        
+
         self.setWindowTitle('PDF & IMAGE Converter')
        # self.setWindowIcon(QIcon('pdf_image.png'))
         self.resize(300, 200)
