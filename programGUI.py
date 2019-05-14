1a5cad2df4688aec15fc5035ce8c9860867c63c8
Chaehyeon Lee <123456ccdd@naver.com>
2019-05-14 17:44:12 +0900

    Update programGUI.py
    Changed the layout from grid to box and added some functions.

--- programGUI.py
+++ programGUI.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QWidget, 
-QPushButton, QDesktopWidget, QHBoxLayout, QGridLayout, QLabel, QLineEdit, QTextEdit)
+QPushButton, QDesktopWidget, QHBoxLayout, QVBoxLayout, QGridLayout, 
+QLabel, QLineEdit, QTextEdit, QRadioButton)
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QCoreApplication, Qt
 
@@ -14,38 +15,69 @@ class MyApp(QWidget):
 
     def initUI(self):
         # set grid layout
-        grid = QGridLayout()
-        self.setLayout(grid)
+        # grid = QGridLayout()
+        # self.setLayout(grid)
 
-
-        # fromBox = QHBoxLayout()
-        # fromBox.addStretch()
-        
+        ''' create from '''
         from_label = QLabel('From')
         from_label.setAlignment(Qt.AlignCenter)
-        to_label = QLabel('to')
-        to_label.setAlignment(Qt.AlignCenter)
-
-        grid.addWidget(from_label, 0, 0)
-        grid.addWidget(to_label, 1, 0)
+        from_lineEdit = QLineEdit()
+        from_lineEdit.setFixedWidth(130)
+        from_open_btn = QPushButton('open')
         
+        fromHBox = QHBoxLayout()
+        fromHBox.addWidget(from_label)
+        fromHBox.addStretch(1)
+        fromHBox.addWidget(from_lineEdit)
+        fromHBox.addStretch(2)
+        fromHBox.addWidget(from_open_btn)
 
+        ''' create to '''
+        to_label = QLabel('To')
+        to_label.setAlignment(Qt.AlignCenter)
+        self.to_same_rbtn = QRadioButton('same directory', self)
+        self.to_same_rbtn.setChecked(True)
+        self.to_same_rbtn.clicked.connect(self.radioButtonClicked)
+        to_other_rbtn = QRadioButton('others', self)
+        to_other_rbtn.clicked.connect(self.radioButtonClicked)
+
+        toHBox = QHBoxLayout()
+        toHBox.addWidget(to_label)
+        toHBox.addStretch(1)
+        toHBox.addWidget(self.to_same_rbtn)
+        toHBox.addStretch(1)
+        toHBox.addWidget(to_other_rbtn)
+
+        ''' create secret (if click the others) '''
+        self.to_lineEdit = QLineEdit()
+        self.to_lineEdit.setFixedWidth(130)
+        to_open_btn = QPushButton('open')
+  
+        secretHBox = QHBoxLayout()
+        secretHBox.addWidget(self.to_lineEdit)
+        secretHBox.addWidget(to_open_btn)
+        
+        ''' create quit button '''
         quit_btn = QPushButton('quit')
         quit_btn.clicked.connect(QCoreApplication.instance().quit)
-        #btn.resize(30, 10)
 
-        grid.addWidget(quit_btn, 2, 2)
 
-        grid.addWidget(QLineEdit(), 0, 1);
-        grid.addWidget(QLineEdit(), 1, 1);
+        quitHBox = QHBoxLayout()
+        quitHBox.addStretch(5)
+        quitHBox.addWidget(quit_btn)
+        # grid.addWidget(quit_btn, 2, 2)
 
-        open_btn = QPushButton('open')
-        grid.addWidget(open_btn, 0, 2)
-        
+        vbox = QVBoxLayout()
+        vbox.addLayout(fromHBox)
+        vbox.addLayout(toHBox)
+        vbox.addLayout(secretHBox)
+        vbox.addLayout(quitHBox)
+
+        self.setLayout(vbox)
 
         self.setWindowTitle('PDF & IMAGE Converter')
        # self.setWindowIcon(QIcon('pdf_image.png'))
-        self.resize(300, 200)
+        self.resize(300, 180)
         self.center()
         self.show()
 
@@ -54,6 +86,13 @@ class MyApp(QWidget):
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+
+    def radioButtonClicked(self):
+        if self.to_same_rbtn.isChecked():
+            self.to_lineEdit.setVisible(False)
+        else:
+            self.to_lineEdit.setVisible(True)
+
 
 if __name__ == '__main__':
 
